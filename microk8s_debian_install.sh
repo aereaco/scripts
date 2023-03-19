8a8fa9f5e0f4a5ea1582e709755d4feccb004716
aereaco <young.vernon@gmail.com>
2023-03-19 16:08:32 -0400

    Create microk8s_debian_install.sh

---
+++ microk8s_debian_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install podman pip cockpit cockpit-podman network-manager snapd
+sudo pip install podman-compose
+
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo swapoff -a
+sudo free -m
+
+sudo systemctl enable --now NetworkManager
+sudo systemctl enable --now podman.socket
+sudo systemctl enable --now cockpit.socket
+
+sudo snap install microk8s --classic
+
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
